e13af22f66663f4a37ec41ea0da6ba80cae48394
Michael Osarumwense <sanmicad@gmail.com>
2024-03-27 00:27:13 +0000

    fixed edit not working

--- src/utils/handlers/editPostHandler.js
+++ src/utils/handlers/editPostHandler.js
@@ -3,15 +3,15 @@ const { getPostById } = require('./getPostHandler');
 const url = process.env.REACT_APP_URL;
 
 export async function EditPostHandler(postId, setDescription, setSpinnerLoading) {
-	try {
-		setSpinnerLoading(true);
+  try {
+    setSpinnerLoading(true);
 
-		const posts = await getPostById(url, postId);
+    const posts = await getPostById(url, postId);
 
-		await setDescription(posts.data.description);
-		await setSpinnerLoading(false);
-	} catch (e) {
-		setSpinnerLoading(true);
-		console.log(e);
-	}
+    await setDescription(posts.description);
+    await setSpinnerLoading(false);
+  } catch (e) {
+    setSpinnerLoading(true);
+    console.log(e);
+  }
 }
