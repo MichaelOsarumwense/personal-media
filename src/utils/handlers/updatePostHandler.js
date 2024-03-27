8b3f529e02f7ea3deb1ac2453a47c1b0d6ecd612
Michael Osarumwense <sanmicad@gmail.com>
2024-03-27 00:48:37 +0000

    fixed update post handler

--- src/utils/handlers/updatePostHandler.js
+++ src/utils/handlers/updatePostHandler.js
@@ -2,26 +2,26 @@ const axios = require('axios');
 const { getToken } = require('../windowsHelper');
 
 const updatePost = async (url, body, postId) => {
-	const response = await axios.request({
-		method: 'PATCH',
-		baseURL: url,
-		url: `/posts/${postId}`,
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: getToken(),
-		},
-		data: body,
-	});
+  const response = await axios.request({
+    method: 'PATCH',
+    baseURL: url,
+    url: `/posts/${postId}`,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: getToken(),
+    },
+    data: body,
+  });
 
-	if (!response.statusText) {
-		return Promise.reject(response);
-	} else {
-		const object = await response;
-		return object;
-	}
+  if (response.status !== 200) {
+    return Promise.reject(response);
+  } else {
+    const object = await response;
+    return object;
+  }
 };
 
 module.exports = {
-	updatePost,
+  updatePost,
 };
