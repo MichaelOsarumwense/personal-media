ace2c0dd7a14aeeed4f803628bb6087a0475f57a
Michael Osarumwense <sanmicad@gmail.com>
2024-05-05 23:46:23 +0100

    added download image function

--- src/components/modal/imageModal.js
+++ src/components/modal/imageModal.js
@@ -1,60 +1,91 @@
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, ButtonGroup } from 'react-bootstrap';
 
 function ImageModals(props) {
-	return (
-		<Modal show={props.show}>
-			<Modal.Header closeButton onClick={props.handleClose}>
-				<Modal.Title>Update Profile Picture</Modal.Title>
-			</Modal.Header>
-			<Modal.Body id="imageModal">
-				<label htmlFor="image">
-					<input
-						className="w3-center"
-						type="file"
-						name="image"
-						id="image"
-						onChange={props.changeHandler}
-					/>
-					{props.isFilePicked ? (
-						<div id="modalDiv">
-							<p id="fileName">Filename: {props.selectedFile.name}</p>
-							<p>Filetype: {props.selectedFile.type}</p>
-							<p>Size in KB: {props.selectedFile.size / 1000}</p>
-							<p>
-								lastModifiedDate:{' '}
-								{props.selectedFile.lastModifiedDate.toLocaleDateString()}
-							</p>
-						</div>
-					) : (
-						<p id="imageModalText">click to upload new image </p>
-					)}
-				</label>
-			</Modal.Body>
-			{props.isFilePicked ? (
-				<Modal.Footer>
-					<Button variant="primary" onClick={props.updateImageHandler} id="updateRef">
-						Update
-					</Button>
-					<Button
-						variant="primary"
-						onClick={props.clear}
-						id="clearRef"
-						style={{ backgroundColor: '#435761', borderColor: '#fff' }}
-					>
-						Clear
-					</Button>
-				</Modal.Footer>
-			) : (
-				<Button
-					style={{ backgroundColor: '#b30f1f', borderColor: '#fff' }}
-					onClick={props.deleteImageHandler}
-					id="removeRef"
-				>
-					Remove Profile Photo
-				</Button>
-			)}
-		</Modal>
-	);
+  const downloadProfileImage = () => {
+    // Create a temporary anchor element
+    const link = document.createElement('a');
+    // Set the href attribute to the URL of the profile image
+    link.href = props.profileImageUrl;
+
+    // Set the download attribute with the desired filename
+    link.download = 'profile-image.jpg';
+    // Append the anchor element to the document body
+    document.body.appendChild(link);
+    // Click the anchor element to trigger the download
+    link.click();
+    // Remove the anchor element from the document body
+    document.body.removeChild(link);
+  };
+
+  const disableDownloadButton = () => {
+    let url = props.profileImageUrl.toString();
+    if (url.startsWith('blob')) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  return (
+    <Modal show={props.show}>
+      <Modal.Header closeButton onClick={props.handleClose}>
+        <Modal.Title>Update Profile Picture</Modal.Title>
+      </Modal.Header>
+      <Modal.Body id="imageModal">
+        <label htmlFor="image">
+          <input
+            className="w3-center"
+            type="file"
+            name="image"
+            id="image"
+            onChange={props.changeHandler}
+          />
+          {props.isFilePicked ? (
+            <div id="modalDiv">
+              <p id="fileName">Filename: {props.selectedFile.name}</p>
+              <p>Filetype: {props.selectedFile.type}</p>
+              <p>Size in KB: {props.selectedFile.size / 1000}</p>
+              <p>lastModifiedDate: {props.selectedFile.lastModifiedDate.toLocaleDateString()}</p>
+            </div>
+          ) : (
+            <p id="imageModalText">click to upload new image </p>
+          )}
+        </label>
+      </Modal.Body>
+      {props.isFilePicked ? (
+        <Modal.Footer>
+          <Button variant="primary" onClick={props.updateImageHandler} id="updateRef">
+            Update
+          </Button>
+          <Button
+            variant="primary"
+            onClick={props.clear}
+            id="clearRef"
+            style={{ backgroundColor: '#435761', borderColor: '#fff' }}
+          >
+            Clear
+          </Button>
+        </Modal.Footer>
+      ) : (
+        <ButtonGroup aria-label="Profile Image Actions">
+          <Button
+            variant="primary"
+            onClick={downloadProfileImage}
+            disabled={disableDownloadButton()}
+          >
+            Download Profile Image
+          </Button>
+          <Button
+            style={{ backgroundColor: '#b30f1f', borderColor: '#fff' }}
+            onClick={props.deleteImageHandler}
+            id="removeRef"
+          >
+            Remove Profile Photo
+          </Button>
+        </ButtonGroup>
+      )}
+    </Modal>
+  );
 }
 
 export default ImageModals;
--- src/components/userInfo/userInfo.js
+++ src/components/userInfo/userInfo.js
@@ -10,121 +10,115 @@ import LoaderComponent from '../loader/loader';
 import ImageModals from '../modal/imageModal';
 
 export function UserInfoLeftColumn(props) {
-	const [userData, setUserData] = useState({});
-	const [avatar, setAvatar] = useState(false);
-	const [show, setShow] = useState(false);
-	const [spinnerLoading, setSpinnerLoading] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [avatar, setAvatar] = useState(defaultImage);
+  const [show, setShow] = useState(false);
+  const [spinnerLoading, setSpinnerLoading] = useState(false);
 
-	const handleShow = () => setShow(true);
-	const handleClose = () => {
-		setShow(false);
-		clearImageModal();
-	};
-	const clearImageModal = () => setIsFilePicked(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => {
+    setShow(false);
+    clearImageModal();
+  };
+  const clearImageModal = () => setIsFilePicked(false);
 
-	const updatedAvatarState = () => getAvatar(setAvatar);
+  const updatedAvatarState = () => getAvatar(setAvatar);
 
-	useEffect(() => {
-		getUserHandler(setUserData);
-		updatedAvatarState();
-	}, []);
+  useEffect(() => {
+    getUserHandler(setUserData);
+    updatedAvatarState();
+  }, []);
 
-	const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
 
-	const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsFilePicked(true);
-	};
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
 
-	const updateImageHandler = () =>
-		updateAvatarHandler(setSpinnerLoading, selectedFile, updatedAvatarState, handleClose);
-
-	return (
-		<div className="w3-col m3 sticky">
-			<LoaderComponent spinnerLoading={spinnerLoading} />
-			<div className="w3-card w3-round w3-white">
-				<div className="w3-container">
-					<Link id="userProfile" to="/update-user">
-						<h4 className="w3-center">
-							<br />
-							<i className="fa fa-edit"></i> Edit Profile
-						</h4>
-					</Link>
-					<p className="w3-center">
-						<label htmlFor="image">
-							<img
-								id="profileImg"
-								src={avatar ? avatar : defaultImage}
-								className="w3-circle"
-								alt="Avatar"
-							/>
-						</label>
-					</p>
-					<Link to={''} onClick={handleShow}>
-						<h4 className="w3-center">Update Image</h4>
-						<hr />
-					</Link>
-					<p id="profileName">
-						<i className="fa fa-user fa-fw w3-margin-right w3-text-theme"></i>
-						{userData.name}
-					</p>
-					<p id="profileAddress">
-						<i className="fa fa-home fa-fw w3-margin-right w3-text-theme"></i>
-						{userData.address}
-					</p>
-					<p id="profileDob">
-						<i className="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i>
-						{userData.dob}
-					</p>
-					<br />
-				</div>
-			</div>
-			<br />
-			{/* <!-- Interests --> */}
-			<div className="w3-card w3-round w3-white w3-hide-small">
-				<div className="w3-container">
-					<br />
-					<p>Hobbies:</p>
-					<p id="profileHobbies">
-						<span className="w3-tag w3-small w3-theme-d5">
-							{props.userData.hobbies}
-						</span>
-					</p>
-					<br />
-				</div>
-			</div>
-			<br />
-			{/* <!-- End Left Column --> */}
-			<ImageModals
-				show={show}
-				handleClose={handleClose}
-				deleteImageHandler={() => {
-					deleteAvatar(defaultAvatar, handleClose, setSpinnerLoading);
-				}}
-				updateImageHandler={updateImageHandler}
-				changeHandler={changeHandler}
-				selectedFile={selectedFile}
-				isFilePicked={isFilePicked}
-				clear={clearImageModal}
-			/>
-		</div>
-	);
+  const updateImageHandler = () =>
+    updateAvatarHandler(setSpinnerLoading, selectedFile, updatedAvatarState, handleClose);
+  const profileImg = avatar ? avatar : defaultImage;
+  return (
+    <div className="w3-col m3 sticky">
+      <LoaderComponent spinnerLoading={spinnerLoading} />
+      <div className="w3-card w3-round w3-white">
+        <div className="w3-container">
+          <Link id="userProfile" to="/update-user">
+            <h4 className="w3-center">
+              <br />
+              <i className="fa fa-edit"></i> Edit Profile
+            </h4>
+          </Link>
+          <p className="w3-center">
+            <label htmlFor="image">
+              <img id="profileImg" src={avatar} className="w3-circle" alt="Avatar" />
+            </label>
+          </p>
+          <Link to={''} onClick={handleShow}>
+            <h4 className="w3-center">Update Image</h4>
+            <hr />
+          </Link>
+          <p id="profileName">
+            <i className="fa fa-user fa-fw w3-margin-right w3-text-theme"></i>
+            {userData.name}
+          </p>
+          <p id="profileAddress">
+            <i className="fa fa-home fa-fw w3-margin-right w3-text-theme"></i>
+            {userData.address}
+          </p>
+          <p id="profileDob">
+            <i className="fa fa-birthday-cake fa-fw w3-margin-right w3-text-theme"></i>
+            {userData.dob}
+          </p>
+          <br />
+        </div>
+      </div>
+      <br />
+      {/* <!-- Interests --> */}
+      <div className="w3-card w3-round w3-white w3-hide-small">
+        <div className="w3-container">
+          <br />
+          <p>Hobbies:</p>
+          <p id="profileHobbies">
+            <span className="w3-tag w3-small w3-theme-d5">{props.userData.hobbies}</span>
+          </p>
+          <br />
+        </div>
+      </div>
+      <br />
+      {/* <!-- End Left Column --> */}
+      <ImageModals
+        show={show}
+        handleClose={handleClose}
+        deleteImageHandler={() => {
+          deleteAvatar(defaultAvatar, handleClose, setSpinnerLoading);
+        }}
+        updateImageHandler={updateImageHandler}
+        changeHandler={changeHandler}
+        selectedFile={selectedFile}
+        isFilePicked={isFilePicked}
+        profileImageUrl={avatar}
+        clear={clearImageModal}
+      />
+    </div>
+  );
 }
 
 export function UserInfoRightColumn(props) {
-	return (
-		<div className="w3-col m2 w3-hide-small sticky">
-			<div className="w3-card w3-round w3-white w3-center">
-				<div className="w3-container">
-					<br />
-					<p>Events:</p>
-					<p id="profileEvents">{props.userData.events}</p>
-					<br />
-				</div>
-			</div>
-			<br />
-			{/* <!-- End Right Column --> */}
-		</div>
-	);
+  return (
+    <div className="w3-col m2 w3-hide-small sticky">
+      <div className="w3-card w3-round w3-white w3-center">
+        <div className="w3-container">
+          <br />
+          <p>Events:</p>
+          <p id="profileEvents">{props.userData.events}</p>
+          <br />
+        </div>
+      </div>
+      <br />
+      {/* <!-- End Right Column --> */}
+    </div>
+  );
 }
